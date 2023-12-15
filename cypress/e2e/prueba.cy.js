688d16507b4499c96b89051b94def4b70d46100a
Julieta Andrea Lopez <julieta.lopez@claro.com.ar>
2023-12-15 10:06:42 -0300

    se comenta los casos de prueba

--- cypress/e2e/prueba.cy.js
+++ cypress/e2e/prueba.cy.js
@@ -3,12 +3,12 @@ describe('template spec', () => {
     cy.visit('https://example.cypress.io')
   })
 
-  it('Test que FALLA', () => {
-    cy.visit('https://example.cypress.io')
-    cy.get("h1").should("have.text","Hola Mundo");
-  })
-  it.skip('Test que NO SE EJECUTA', () => {
-    cy.visit('https://example.cypress.io')
+  // it('Test que FALLA', () => {
+  //   cy.visit('https://example.cypress.io')
+  //   cy.get("h1").should("have.text","Hola Mundo");
+  // })
+  // it.skip('Test que NO SE EJECUTA', () => {
+  //   cy.visit('https://example.cypress.io')
     
-  })
+  // })
 })
