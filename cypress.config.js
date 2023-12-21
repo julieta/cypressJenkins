1647c37f980c89a144bfeeeb4a57232a7c8319b4
Julieta Andrea Lopez <julieta.lopez@claro.com.ar>
2023-12-21 09:05:59 -0300

    Incorporación del archivo jenkinsfile

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "report",
+    reportFileName: "my_new_report"
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
